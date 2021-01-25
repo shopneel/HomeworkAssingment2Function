881bb5f9fc6d28ef9da749833581744e88e299b7
shopneel <77643192+shopneel@users.noreply.github.com>
2021-01-25 02:15:58 +0000

    Add files via upload

---
+++ Homework Assingment#2 Function.py
@@ -0,0 +1,27 @@
+
+
+
+# Bohaimean Rhapsody
+
+def Genre_Artist_Year_Boolean(name1, name2, number, bool):
+    output = name1 + name2 + str(number) + str(bool)
+    return output
+
+
+song = Genre_Artist_Year_Boolean("Hard Rock", " Fredy Mercury ", 1975, True)
+
+
+print(song)
+
+
+def Bohaimean_Rhapsody(name1, name2, number, bool):
+    output = name1 + name2 + str(number) + str(bool)
+    return output
+
+genre = name1 = ("Hard Rock ")
+artist = name2 = ("Fredy Mercury ")
+year = number = 1975
+bool = False or True
+
+print(Bohaimean_Rhapsody(name1, name2, number, bool))
+
